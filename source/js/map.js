77e77132357fab361bd628a76682c59adb286ae6
Kristina Z <zakharova.kr@gmail.com>
2019-05-01 00:33:47 -0400

    поработала над картами и страницей каталога

--- source/js/map.js
+++ source/js/map.js
@@ -1,8 +1,18 @@
-var map;
+ymaps.ready(init);
 
 function init() {
-    map = google.maps.Map(document.getElementById('map'), {
-        center: {lat: 59.938577, lng: 30.322857},
-        zoom: 18
-    });
+  var map = new ymaps.Map('map', {
+    center: [59.938631, 30.323055],
+    zoom: 18,
+    controls: []
+  });
+
+  var placemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/map-pin.png',
+    iconImageSize: [55, 53],
+    iconImageOffset: [0, -70],
+  });
+
+  map.geoObjects.add(placemark);
 }
